c23a0897bc667e3a72d0611e9d1eab14b2d668e1
nopecho <ch.jooon@gmail.com>
2024-01-07 02:46:58 +0900

    testcontainers

--- schema/health.sh
+++ schema/health.sh
@@ -1,11 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 
-function is_elastic_server_up() {
+is_elastic_server_up() {
+    # shellcheck disable=SC2039
     local server_url=$1
-    curl --silent --fail "$server_url"/_cluster/health && return 0 || return 1
+    if curl --silent --fail "$server_url"/_cluster/health; then
+        return 0
+    else
+        return 1
+    fi
 }
 
-function wait_for_elastic() {
+wait_for_elastic() {
+    # shellcheck disable=SC2039
     local server_url=$1
     while true; do
         if is_elastic_server_up "$server_url"; then
@@ -16,12 +22,16 @@ function wait_for_elastic() {
     done
 }
 
-function is_aws_server_up() {
+is_aws_server_up() {
     local server_url=$1
-    curl --silent --fail "$server_url"/_localstack/health && return 0 || return 1
+    if curl --silent --fail "$server_url"/_localstack/health; then
+        return 0
+    else
+        return 1
+    fi
 }
 
-function wait_for_aws() {
+wait_for_aws() {
     local server_url=$1
     while true; do
         if is_aws_server_up "$server_url"; then
@@ -36,7 +46,7 @@ LOCAL_HOST="http://localhost:"
 ES_PORT="9222"
 AWS_PORT="4569"
 
-wait_for_elastic $LOCAL_HOST$ES_PORT
-wait_for_aws $LOCAL_HOST$AWS_PORT
+wait_for_elastic "$LOCAL_HOST$ES_PORT"
+wait_for_aws "$LOCAL_HOST$AWS_PORT"
 
 echo "Both servers are running. Exiting."
